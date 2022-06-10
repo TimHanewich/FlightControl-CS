ed105f1afe9788016b591301e581380bb6851b17
Tim Hanewich <tahanewich@live.com>
2022-06-10 18:47:14 -0400

    Added status light controller

---
+++ Components/StatusLightController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+
+namespace FlightControl
+{
+    public class StatusLightController
+    {
+        public async Task StartAsync()
+        {
+            GpioController gpio = new GpioController();
+            int pin = 18;
+            gpio.OpenPin(pin, PinMode.Output);
+
+            while (true)
+            {
+                if (FlightControl.Instance.SystemStatus == Status.Offline)
+                {
+                    gpio.Write(pin, PinValue.High);
+                    await Task.Delay(1500);
+                    gpio.Write(pin, PinValue.Low);
+                    await Task.Delay(1500);
+                }
+                else if (FlightControl.Instance.SystemStatus == Status.StandBy)
+                {
+                    gpio.Write(pin, PinValue.High);
+                    await Task.Delay(250);
+                    gpio.Write(pin, PinValue.Low);
+                    await Task.Delay(250);
+                    gpio.Write(pin, PinValue.High);
+                    await Task.Delay(250);
+                    gpio.Write(pin, PinValue.Low);
+                    await Task.Delay(2500);
+                }
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Device.Gpio;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace FlightControl
 {
@@ -8,6 +10,25 @@ namespace FlightControl
         public static void Main(string[] args)
         {
             
+            //Start the controller
+            StatusLightController slc = new StatusLightController();
+            Task.Run(() => slc.StartAsync()); //Run in another thread
+
+            while (true)
+            {
+                Console.Write("Making it offline... ");
+                FlightControl.Instance.SystemStatus = Status.Offline;
+                Console.WriteLine("Offline!");
+                Task.Delay(10000).Wait();
+                Console.Write("Making it on standby... ");
+                FlightControl.Instance.SystemStatus = Status.StandBy;
+                Console.WriteLine("Standby!");
+                Task.Delay(10000).Wait();
+            }
+
+
+
+
         }
     }
 }
